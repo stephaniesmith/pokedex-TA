c1b967a70e2e99561326e16a1e23fa1491fe9807
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-15 19:00:01 -0700

    created header component

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header id="header">
+    <h1>Header</h1>
+    <Filters/>
+    <Sort/>
+  </header>
+</template>
+
+<script>
+  import Filters from './Filters.vue';
+  import Sort from './Sort.vue';
+
+  export default {
+    components: {
+      Filters,
+      Sort
+    }
+  }
+</script>
+
+<style scoped>
+  #header {
+    background-color: cornflowerblue
+  }
+</style>
