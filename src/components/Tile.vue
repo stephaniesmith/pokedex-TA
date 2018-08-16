3f23250cef56f74fb9ecc59b888e6524500e0d33
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-15 19:00:28 -0700

    created tile component

---
+++ src/components/Tile.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="pokemon" :style="{ backgroundImage: 'url(' + pokemon.url_image + ')' }">
+    <h2>{{pokemon.pokemon}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['pokemon']
+  }
+</script>
+
+<style>
+  #pokemon {
+    background-position: center;
+    background-repeat: none;
+    background-size: cover;
+    min-height: 300px;
+    border: 15px solid white;
+  }
+</style>
