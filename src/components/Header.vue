335413967b18f418a6c4a04e9ba9ef44fd52ffd2
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-17 20:35:26 -0700

    Styling the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,27 +1,41 @@
 <template>
   <header id="header">
-    <h1>Header</h1>
-    <Filters :filter="filter" :types="types"/>
-    <Sort :sort="sort"/>
+    <h1>Pokédex</h1>
+    <div class="sort-filter">
+      <Filters :filter="filter" :types="types"/>
+      <Sort :sort="sort"/>
+    </div>
   </header>
 </template>
 
 <script>
-  import Filters from './Filters.vue';
-  import Sort from './Sort.vue';
+import Filters from './Filters.vue';
+import Sort from './Sort.vue';
 
-  export default {
-    components: {
-      Filters,
-      Sort
-    },
-    props: ['types', 'filter', 'sort']
-  }
+export default {
+  components: {
+    Filters,
+    Sort
+  },
+  props: ['types', 'filter', 'sort']
+};
 </script>
 
 <style scoped>
   #header {
-    background-color: cornflowerblue;
+    background-color: rgb(235, 0, 0);
     height: 100px;
+  }
+
+  #header h1 {
+    padding-top: .5em;
+    font-size: 2.5em;
+    font-weight: bolder;
+  }
+
+  .sort-filter {
+    display: flex;
+    justify-content: center;
+    width: 100vw;
   }
 </style>
